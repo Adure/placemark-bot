8e7020327ecd3b0ab8b5a2ab8514d2d5f97c718c
Aran Wittert <aran@jobadder.com>
2020-03-17 12:17:54 +1100

    basic bot

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>')
+
+#Implement algo to determine if an account is an egirl or not
+@bot.command()
+async def egirl(ctx, name):
+    pass
+	
+#Implement algo to determine if an account is a onetrick or not
+@bot.command()
+async def onetrick(ctx, name):
+    pass
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+	
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    print(message)
+
+bot.run('token')
